8571a5bc7f4a317f9dc3088edecce26d32e84b38
ilspsolucoes-cell <ilspsolucoes@gmail.com>
2026-06-02 21:34:01 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+exports.handler = async () => ({
+  statusCode: 200,
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({
+    apiKey:            process.env.FB_API_KEY,
+    authDomain:        process.env.FB_AUTH_DOMAIN,
+    databaseURL:       process.env.FB_DATABASE_URL,
+    projectId:         process.env.FB_PROJECT_ID,
+    storageBucket:     process.env.FB_STORAGE_BUCKET,
+    messagingSenderId: process.env.FB_SENDER_ID,
+    appId:             process.env.FB_APP_ID,
+  }),
+});
